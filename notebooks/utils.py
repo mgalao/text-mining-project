b9c5aeb244666aaafe337452a22bc3a45c1b02a7
brunagcsimoes <20240491@novaims.unl.pt>
2025-06-11 18:33:33 +0100

    Update utils.py

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -19,6 +19,30 @@ from tqdm import tqdm
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import gensim.downloader
+import pickle
+#import packages
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense, TimeDistributed, Bidirectional, Masking
+from keras import layers
+import tensorflow as tf
+import tensorflow.keras as keras
+from keras_preprocessing.sequence import pad_sequences
+from scipy import sparse
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import MultinomialNB
+from gensim.models import Word2Vec
+from keras_preprocessing.sequence import pad_sequences
+from sklearn.utils.class_weight import compute_class_weight
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
 
 def get_top_words_by_class(df, label_col, text_col, top_criteria=10):
     result = []
@@ -145,6 +169,106 @@ def clean_text(text_list, lemmatize=True, stem=False):
 
     return cleaned
 
+
+# Function to convert tokens into mean embedding vector
+def tweet_to_vec(tokens, model, size):
+    vectors = [model.wv[word] for word in tokens if word in model.wv]
+    return np.mean(vectors, axis=0) if vectors else np.zeros(size)
+
+#get word embeddings from documents
+def corpus2vec(corpus, glove_model):
+    index_set = set(glove_model.index_to_key)  # fast lookup
+    word_vec = glove_model.get_vector          # alias for speed
+
+    return [
+        [word_vec(word) for word in word_tokenize(doc) if word in index_set]
+        for doc in tqdm(corpus)
+    ]
+
+def average_tweet_vectors(corpus_vectors, vector_size):
+    return np.array([
+        np.mean(tweet, axis=0) if len(tweet) > 0 else np.zeros(vector_size)
+        for tweet in corpus_vectors
+    ])
+
+# embeddings
+def embedding_bow(x_train, y_train, x_val, model, ngram_range=(1,1)):
+    bow = CountVectorizer(binary=True, ngram_range=ngram_range) # each term is marked as present or not per document - good for short text
+    X_bow = bow.fit_transform(x_train['text'])
+
+    model.fit(X_bow,y_train)
+
+    y_train_pred = model.predict(bow.transform(x_train['text']))
+    y_val_pred = model.predict(bow.transform(x_val['text']))
+
+    return X_bow, y_train_pred, y_val_pred, bow
+
+def embedding_tfidf(x_train, y_train, x_val, model, max_df, ngram_range=(1,1)):
+    tfidf = TfidfVectorizer(max_df=max_df, ngram_range=ngram_range) 
+    X_tfidf = tfidf.fit_transform(x_train['text']).toarray()
+
+    model.fit(X_tfidf,y_train)
+
+    y_train_pred = model.predict(tfidf.transform(x_train['text']))
+    y_val_pred = model.predict(tfidf.transform(x_val['text']))
+
+    return X_tfidf, y_train_pred, y_val_pred, tfidf
+
+def embedding_word2vec(x_train, y_train, x_val, window, min_count, model, vector_size=None, sg=1):
+    tokenized_train = [word_tokenize(tweet.lower()) for tweet in x_train['text']]
+    tokenized_val = [word_tokenize(tweet.lower()) for tweet in x_val['text']]
+
+    corpus = x_train['text']
+
+    #get list with lenghts of sentences
+    train_len = []
+    for i in corpus:
+        train_len.append(len(i))
+
+    if vector_size == None:
+        word2vec = Word2Vec(
+            sentences=tokenized_train,
+            vector_size=max(train_len),    # size of the embedding vectors 
+            window=window,        
+            min_count=min_count,     
+            sg=sg            
+        )
+    else:
+        word2vec = Word2Vec(
+            sentences=tokenized_train,
+            vector_size=vector_size,   
+            window=window,        
+            min_count=min_count,     
+            sg=sg            
+        )
+
+    # Apply to train and validation sets
+    X_train_vec = np.array([tweet_to_vec(tokens, word2vec, word2vec.vector_size) for tokens in tokenized_train])
+    X_val_vec = np.array([tweet_to_vec(tokens, word2vec, word2vec.vector_size) for tokens in tokenized_val])
+
+    model.fit(X_train_vec,y_train)
+
+    y_train_pred = model.predict(X_train_vec)
+    y_val_pred = model.predict(X_val_vec)
+
+    return X_train_vec, y_train_pred, y_val_pred
+
+def embedding_glove(x_train, y_train, x_val, model_glove, emb_size, model):
+    x_train = corpus2vec(x_train['text'], model_glove)
+    x_val = corpus2vec(x_val['text'], model_glove)
+
+    X_train_avg = average_tweet_vectors(x_train, emb_size)
+    X_val_avg = average_tweet_vectors(x_val, emb_size)
+
+    model.fit(X_train_avg, y_train)
+
+    y_pred = model.predict(X_val_avg)
+
+    return X_train_avg, y_pred
+
+
+# Metrics
+
 def compute_metrics(y_true, y_pred):
     """
     Compute F1, precision, recall, and accuracy for given true and predicted labels.
@@ -186,6 +310,9 @@ def get_metrics_df(model_name, y_train, y_train_pred, y_val, y_val_pred):
 
     return pd.DataFrame([data])
 
+
+# Plots
+
 def plot_metrics(y_train, y_train_pred, y_val, y_val_pred, title="Model Performance"):
     """
     Plots accuracy, precision, recall, and F1 score for train and validation sets.
@@ -211,8 +338,8 @@ def plot_metrics(y_train, y_train_pred, y_val, y_val_pred, title="Model Performa
     plt.figure(figsize=(9, 5))
 
     # Plot bars
-    train_bars = plt.bar(x, train_scores, width=bar_width, label='Train', color='#5c676c')
-    val_bars = plt.bar([i + bar_width for i in x], val_scores, width=bar_width, label='Validation', color='#b9d706')
+    train_bars = plt.bar(x, train_scores, width=bar_width, label='Train', color='#1f4e79')
+    val_bars = plt.bar([i + bar_width for i in x], val_scores, width=bar_width, label='Validation', color='#A9C4E2')
 
     # Add values on top of bars
     for bars in [train_bars, val_bars]:
@@ -227,5 +354,21 @@ def plot_metrics(y_train, y_train_pred, y_val, y_val_pred, title="Model Performa
     plt.title(title)
     plt.legend()
     plt.grid(axis='y', linestyle='--', alpha=0.6)
+    plt.tight_layout()
+    plt.show()
+
+def plot_confusion_matrix(y_true, y_pred, title="Confusion Matrix", labels=None, figsize=(6, 5), cmap="YlGnBu"):
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+
+    plt.figure(figsize=figsize)
+    sns.heatmap(cm, annot=True, fmt='d', cmap=cmap, 
+                xticklabels=labels, yticklabels=labels,
+                linewidths=0, linecolor='white', cbar=False)
+    
+    plt.title(title, fontsize=14, fontweight='bold')
+    plt.xlabel("Predicted Label", fontsize=12)
+    plt.ylabel("True Label", fontsize=12)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
     plt.tight_layout()
     plt.show()
